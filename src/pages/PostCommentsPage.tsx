685d6d212890611877be9581e91306da90532523
DaniilGamers <ss.mihalkivd@gmail.com>
2024-06-16 02:33:53 +0200

    editing HW_13 complete

--- src/pages/PostCommentsPage.tsx
+++ src/pages/PostCommentsPage.tsx
@@ -7,8 +7,8 @@ const PostCommentsPage = () => {
     return (
         <div>
             {
-                posts.map(post => <div key={post.id}> <ul><h2>Post of title</h2> {post.title}</ul><br/><ul><h2>Comments</h2></ul><br/>{
-                    comments.map(comment => <div key={comment.id}><ul><li><h3>{comment.body}</h3> <h4>{comment.email}</h4></li></ul></div> )
+                posts.map(post => <div key={post.id}> <ul><br/><h2>Post of title</h2> {post.title}</ul><br/><ul><h2>Comments</h2></ul><br/>{
+                    comments.map(comment => <div key={comment.id}><ul><li><h3>{comment.body}</h3></li></ul></div> )
                 }</div>)
             }
         </div>
